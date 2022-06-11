d87a3a87daef3046410bf661da21adbbe39b309f
Luidenka <87653308+Luidenka-Golden@users.noreply.github.com>
2022-06-11 12:11:31 +0800

    Modify files

--- canary.js
+++ canary.js
@@ -10,6 +10,23 @@ function newEditor() {
     lineContent.spellcheck = false;
     lineContent.contentEditable = true;
 
+    lineContent.addEventListener("input", function(e) {
+
+        // Showing line numbers
+        document.getElementById("line-number").innerHTML = "";
+        const lineCount = document.getElementById("line-content").innerText.split("\n").length;
+        if (lineCount != 1) {
+            isNewLine = true;
+            for (let l = 0; l < lineCount; l++) {
+                if (l != 0) {
+                    document.getElementById("line-number").innerText += l;
+                    document.getElementById("line-number").innerText += "\n";
+                }
+            }
+        }
+        else document.getElementById("line-number").innerText = 1;
+    });
+
     editor.appendChild(lineNumber);
     editor.appendChild(lineContent);
     document.body.appendChild(editor);
