7e438c5bbd148f549f3615c8de2e3e2f4e63e478
Luidenka <jarvislui1128@gmail.com>
2022-06-11 13:18:00 +0800

    Auto Close Quotes/Braces

--- canary.js
+++ canary.js
@@ -26,6 +26,37 @@ function newEditor() {
         else lineNumber.innerText = 1;
     });
 
+    lineContent.addEventListener("keypress", function(e) {
+        var charTyped = String.fromCharCode(e.which);
+        if (charTyped == "{" || charTyped == "(" || charTyped == "[" || charTyped == "\"" || charTyped == "'") {
+            
+            e.preventDefault();
+            
+            var sel = window.getSelection();
+            if (sel.rangeCount > 0) {
+                
+                var range = sel.getRangeAt(0);
+                range.deleteContents();
+                
+                
+                var text;
+                if (charTyped == "{") text = "{}"
+                else if (charTyped == "[") text = "[]"
+                else if (charTyped == "\"") text = "\"\""
+                else if (charTyped == "'") text = "''"
+                else text = "()"
+                var textNode = document.createTextNode(text);
+                range.insertNode(textNode);
+                
+                
+                range.setStart(textNode, 1);
+                range.setEnd(textNode, 1);
+                sel.removeAllRanges();
+                sel.addRange(range);
+            }
+        }                                                                                                                                                                                                                                                                                                                                                                                                                           
+    }, false);
+
     editor.appendChild(lineNumber);
     editor.appendChild(lineContent);
     document.body.appendChild(editor);
