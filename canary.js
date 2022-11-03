710be62dfa017fd599f9e62284492a15411944e1
Luidenka <87653308+Luidenka@users.noreply.github.com>
2022-11-03 13:53:36 +0800

    Update canary.js
    New function: `newEditorWithElement(el)`

--- canary.js
+++ canary.js
@@ -114,6 +114,74 @@ function restoreSelection(containerEl, savedSel) {
     sel.addRange(range);
 }
 
+function newEditorWithElement(el) {
+    var editor = document.getElementById(el);
+    var lineNumber = document.createElement("div");
+    var lineContent = document.createElement("div");
+
+    editor.id = "canary-editor";
+    editor.className = "language-python";
+    lineNumber.id = "line-number";
+    lineNumber.innerText = "1";
+    lineContent.id = "line-content";
+    lineContent.spellcheck = false;
+    lineContent.contentEditable = true;
+
+    lineContent.addEventListener("input", () => {
+        // Showing line numbers
+        lineNumber.innerHTML = "";
+        const lineCount = lineContent.innerText.split("\n").length;
+        if (lineCount != 1) {
+            isNewLine = true;
+            for (let l = 0; l < lineCount; l++) {
+                if (l != 0) {
+                    lineNumber.innerText += l;
+                    lineNumber.innerText += "\n";
+                }
+            }
+        }
+        else lineNumber.innerText = 1;
+
+    });
+
+    lineContent.addEventListener("keypress", (e) => {
+        var charTyped = String.fromCharCode(e.which);
+        if (charTyped == "{" || charTyped == "(" || charTyped == "[" || charTyped == "\"" || charTyped == "'") {
+
+            e.preventDefault();
+
+            var sel = window.getSelection();
+            if (sel.rangeCount > 0) {
+
+                var range = sel.getRangeAt(0);
+                range.deleteContents();
+                var text;
+
+                if (charTyped == "{") text = "{}"
+                else if (charTyped == "[") text = "[]"
+                else if (charTyped == "\"") text = "\"\""
+                else if (charTyped == "'") text = "''"
+                else text = "()"
+                var textNode = document.createTextNode(text);
+                range.insertNode(textNode);
+
+
+                range.setStart(textNode, 1);
+                range.setEnd(textNode, 1);
+                sel.removeAllRanges();
+                sel.addRange(range);
+            }
+        } else if (e.key == "Tab") {
+            e.preventDefault();
+
+            window.getSelection().getRangeAt(0).insertNode(document.createTextNode("\t"));
+        }
+    });
+
+    editor.appendChild(lineNumber);
+    editor.appendChild(lineContent);
+}
+
 function syntaxHighlight(el) {
     var text = el.innerText;
 
